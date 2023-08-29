14b91e8eeaa568a1faad020ea74b8dc6e7911f55
chenfudong <260431910@qq.com>
2023-08-29 10:18:31 +0800

    完善pull命令

--- ssprompt/core/prompthub/abstract_prompthub.py
+++ ssprompt/core/prompthub/abstract_prompthub.py
@@ -3,24 +3,45 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Any, Callable, List, Mapping, Optional, Sequence, Union
 from pathlib import Path
+from pydantic import BaseModel, Field, validator
 
-class AbstractPromptHub(ABC):
-    def __init__(self, project: str, sub_project: str, path:Path) -> None:
-        self._main_project = project
-        self._sub_project = sub_project
-        self._path = path 
+class AbstractPromptHub(BaseModel,ABC):
+    main_project: str
+    sub_project: str 
+    path: Path
 
-    @property
-    def main_project(self) -> str:
-        return self._main_project
+    # def __init__(self, project: str, sub_project: str, path:Path) -> None:
+    #     self._main_project = project
+    #     self._sub_project = sub_project
+    #     self._path = path  
     
-    @property
-    def sub_project(self) -> str:
-        return self._sub_project
+    @validator('main_project')
+    def valid_main_project(cls, values: str) -> str:
 
-    @property
-    def path(self) -> Path:
-        return self._path
+        """eg. ptonlix/PromptHub"""
+        count = values.count("/") 
+        #  小于1, 说明不是一个主仓库
+        if  count != 1 :
+            raise ValueError("The name isn't PromptHub project, please check")
+        return values
+
+    @validator('path')
+    def valid_path(cls, value: Path) ->Path:
+        if not value.is_dir():
+            raise ValueError("The Path isn't directory")
+        return value
+
+    # @property
+    # def main_project(self) -> str:
+    #     return self._main_project
+    
+    # @property
+    # def sub_project(self) -> str:
+    #     return self._sub_project
+
+    # @property
+    # def path(self) -> Path:
+    #     return self._path
 
     @abstractmethod
     def check_project_exists(self) -> bool:
@@ -35,6 +56,6 @@ class AbstractPromptHub(ABC):
         ...
     
     @abstractmethod
-    def check_dependencies(self, install: Optional[Callable[[Any], Any]]):
+    def get_project_dependencies(self, install: Optional[Callable[[Any], Any]]):
         ...
 
--- ssprompt/core/prompthub/git_prompthub.py
+++ ssprompt/core/prompthub/git_prompthub.py
@@ -6,12 +6,14 @@ from pathlib import Path
 
 from numpy import savez_compressed
 from ssprompt.core.prompthub.abstract_prompthub import AbstractPromptHub
+from ssprompt.core.config import PyYaml, Config
 import requests
 from urllib.parse import urlparse
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field, validator, PrivateAttr
 import os
 import logging
 import time
+from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
@@ -59,32 +61,28 @@ class GitModel(BaseModel):
 
 class GitPromptHub(AbstractPromptHub):
 
+    _platfrom: Dict =  PrivateAttr()
+    _session: Any = PrivateAttr()
+    _dir_flag: bool = PrivateAttr()
+
+    platform:  Dict[str,Dict[Any, Any]] = {
+        "github":{
+            "repos": "https://api.github.com/repos/{repo_url}",
+            "content": "https://api.github.com/repos/{repo_url}/contents/{file_path}"
+        },
+        "gitee":{}
+    }
+
     def __init__(self, git_type:str, main_project:str, path:Path, sub_project: str="", dir_flag:bool = True) -> None:
-        self._gitmodel = GitModel(main_project=main_project, sub_project=sub_project, path=path)
-        super().__init__(main_project, sub_project, path)
-        if not git_type in self._gitmodel.platform.keys():
+        super().__init__(main_project=main_project, sub_project=sub_project, path=path)
+        if not git_type in self.platform.keys():
              raise ValueError("The Git Type is incorrect, [github or gitee]")  
-        self._platfrom = self._gitmodel.platform.get(git_type, {})
+        self._platfrom = self.platform.get(git_type, {})
         self._session = requests.Session()
         self._dir_flag = dir_flag
 
-
-    def check_project_exists(self) -> bool:
-        repo_url = self.main_project
-        github_api_url = self._platfrom.get("repos", "")
-
-        response = requests.get(github_api_url.format(repo_url=repo_url))
-        return response.status_code == 200
-    
-    def get_project_meta(self)->Any:
-        meta_file = ""
-        if not self.sub_project:
-            meta_file = self.main_project.split("/")[1] + ".yaml"
-        else:
-            meta_file = self.sub_project.split("/")[-1] + ".yaml"
-        
-
-    def pull_project(self):
+    @property
+    def _save_path(self)->Path:
         save_path = None
         if self._dir_flag:
             if not self.sub_project:
@@ -94,13 +92,59 @@ class GitPromptHub(AbstractPromptHub):
             save_path = self.path.joinpath(dir_name)
         else:
             save_path = self.path
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
-        self._download_github_directory(self.sub_project, save_path)
+        return save_path 
+
+    def check_project_exists(self) -> bool:
+        repo_url = self.main_project
+        github_api_url = self._platfrom.get("repos", "")
+
+        response = requests.get(github_api_url.format(repo_url=repo_url))
+        return response.status_code == 200
     
-    def check_dependencies(self, install: Optional[Callable[[Any], Any]]):
-        ...
+    def get_project_meta(self)->Config:
+        meta_file = ""
+        if not self.sub_project:
+            meta_file = self.main_project.split("/")[1] + ".yaml"
+        else:
+            meta_file = self.sub_project.split("/")[-1] + ".yaml"
+        meta_file = self._save_path.joinpath(meta_file)
+        return PyYaml(meta_file).read_config_from_yaml()
     
+    def pull_project(self): 
+        if not os.path.exists(self._save_path):
+            os.makedirs(self._save_path)
+        self._download_github_directory(self.sub_project, self._save_path)
+    
+    def get_project_dependencies(self) ->List[Dict]:
+        depend_list = []
+        meta_obj = self.get_project_meta()
+        if meta_obj.json_prompt:
+            for sub_prompt in meta_obj.json_prompt["list"]:
+                depend_list.append(sub_prompt.get("dependencies"))
+        if meta_obj.yaml_prompt:
+            for sub_prompt in meta_obj. yaml_prompt["list"]:
+                depend_list.append(sub_prompt.get("dependencies")) 
+        if meta_obj.python_prompt:
+            for sub_prompt in meta_obj. python_prompt["list"]:
+                depend_list.append(sub_prompt.get("dependencies")) 
+        return self._set_dependencies_list(depend_list)
+
+    """
+        列表内去重字典
+    """                   
+    def _set_dependencies_list(self, depend:List[Dict]) -> List[Dict]:
+        import json
+        unique_data = []
+        seen_values = set()
+
+        for item in depend:
+            item_str = json.dumps(item, sort_keys=True)
+            if item_str not in seen_values:
+                unique_data.append(item)
+                seen_values.add(item_str) 
+        return unique_data
+
+                    
     def _parse_github_response(self, response):
         return response.json()
 
@@ -111,7 +155,7 @@ class GitPromptHub(AbstractPromptHub):
 
         if response.status_code == 200:
             content_list = self._parse_github_response(response)
-            for item in content_list:
+            for item in tqdm(content_list, desc=f"Downloading {directory_path}", unit="file"):
                 item_name = item['name']
                 item_download_url = item['download_url']
                 item_sha = item['sha']
@@ -127,16 +171,8 @@ class GitPromptHub(AbstractPromptHub):
                 else:
                     self._download_file(item_download_url, item_save_path, item_sha)
         else:
-            print(f"Failed to fetch directory content, Status code: {response.status_code}")
-
-    # def _download_file(self, url, save_path):
-    #     response = self._session.get(url)
-    #     if response.status_code == 200:
-    #         with open(save_path, 'wb') as file:
-    #             file.write(response.content)
-    #         print(f"Downloaded {url} to {save_path}")
-    #     else:
-    #         print(f"Failed to download {url}, Status code: {response.status_code}")
+            logger.error(f"Failed to fetch directory content, Status code: {response.status_code}")
+            logger.error(f"{response.text}") 
 
     def _download_file(self, url, save_path, sha, max_retries:int=3, retry_delay: int=200):
         if os.path.exists(save_path):
@@ -156,12 +192,12 @@ class GitPromptHub(AbstractPromptHub):
                 if not self._check_file_sha(save_path, sha):
                     raise ValueError(f"The downloaded {url} file does not match the expected SHA value. \nPlease check and try again")  
             else:
-                print(f"Failed to download {url}, Status code: {response.status_code}")
+                logger.warning(f"Failed to download {url}, Status code: {response.status_code}")
                 if attempt < max_retries:
-                    print(f"Retrying in {retry_delay} millisecond...")
+                    logger.warning(f"Retrying in {retry_delay} millisecond...")
                     time.sleep(retry_delay/1000)
                 else:
-                    print("Max retry attempts reached.")
+                    logging.error("Max retry attempts reached.\n Please check and try again")
                     break
     
     def _check_file_sha(self, file_path: Path, sha) -> bool:
@@ -183,3 +219,4 @@ if __name__ == "__main__":
 
    print(gitprompthub.check_project_exists())
    gitprompthub.pull_project()
+   print(gitprompthub.get_project_dependencies())
